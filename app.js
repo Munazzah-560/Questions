caf00bb770124d36cd8962fbbcf4fcee88fa5453
Lab Laptop <rams.dai230@gmail.com>
2024-09-09 04:44:44 -0700

    all files

---
+++ app.js
@@ -0,0 +1,184 @@
+// chapter 1
+// Question 1
+
+// alert("Hello")
+
+// Question 2
+//  alert(" JavaScript Alert  \n \n Error! Please enter a valid password")
+
+//  Question 3
+// alert(" JavaScript Alert \n \n Welcome to JS land  \n Happy coding")
+// Question 4 part a
+
+// alert(" JavaScript Alert \n \n Welcome to JS land")  
+// Question 4 part b
+
+// alert(" JavaScript Alert \n \n Happy Coding! \n Prevent this page from creating additional dialogs ") 
+
+// Question 5
+// var name =(" JavaScript Alert \n \n \n")
+// var name2 =("Hello...! I can run JS through my web browsers console ")
+// console.log(name + name2)
+
+// Question 6
+// alert("Hello")
+
+// CHAPTER 2
+
+// Question 1
+// var username 
+
+// Question 2
+// var myName = ("Munazzah Ramzani")
+// console.log(myName)
+
+// Question 3
+//  var message = ("Hello World")
+//  document.write(message)
+//  alert("Hello World")
+
+//  Question 4
+// alert("Jhon doe")
+// alert("15 years old")
+// alert("Certifies App develoment")
+
+// Question 5
+//alert("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// Question 6
+//var email =("my email is example@example.com")
+//console.log(email)
+
+//Question 7
+//var book =("A Smarter way How to Learn JavaScript")
+//console.log(book)
+//alert("I am trying to learn from the Book A Smarter way to learn JavaScript")
+
+//Question 8
+// document.write("Yah!I can write HTML content through JavaScript")
+
+//Question 9
+// alert("------------------")
+// document.write("---------------")
+
+//Chapter 3
+
+//Question 1
+//var age =
+//console.log(age)
+//alert("I am 15 Years old")
+
+//Question 2
+
+//alert("You have visited this site 14 times")
+//document.write("You have visited this site 14 times")
+//var name ="You have visited this site 14 times"
+//console.log(name)
+
+//Question 3
+
+//var birthYear =
+//document.write("My birth year is 1990 <br>Data type of my declared variable is number")
+
+//Question 4
+
+//document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+
+//Chapter 4
+
+//Question 1
+
+// var first_name="20",second_name="30",last_name="50"
+// console.log(first_name,second_name,last_name)
+
+//Question 2
+
+//legal
+// 1.must start with a Letter 
+// 2.underscore_
+// 3.myvar 
+// 4.A variable name can't contain any spaces.
+// 5.shorter name 
+
+//illegal
+// 1.variable names cannot start with a Number
+// 2.variable names cannot contain hyphens 
+// 3.variable names cannot start with special characters 
+// 4.variable names cannot be the same 
+// 5.variable names cannot have spaces
+ 
+//Question 3
+
+// document.write("<b>Rules for naming JS variables</b>")
+// document.write("<br><br><br>Variable names can only contain Number,$ and_")
+// document.write("<br>Variables must begin with a letter ,_name and $")
+// document.write("<br>Variables names are case sensitive")
+// document.write("<br>Variable names should beJS Keywords")
+
+//Chapter 1(Alerts)
+
+// alert("Munazzah")
+// alert("Ahmed Ramzani")
+// alert("abc.de123@gmail.com")
+// alert("0300000000")
+// alert("123abc@!ABC")
+
+//Question 2
+// alert("you're learning JavaScrip!");
+
+//Question 3
+//alert("Hey there!Iam learning JavaScript")
+
+//Chaptr 2(Vriables for String)
+
+//Questin 1
+// var firstWord = "Hello";
+
+//Question 2
+// var name=
+// var second_statement = "Hello World"
+// console.log(second_statement)
+
+//Quetion 3
+// var teamName=
+// alert("Our tem name is:'temName'");
+
+//Question 4
+
+// var bestMan = "Hazrat Muhammad"
+// console.log(bestMan)
+
+//Chapter 4(Variables for numbers)
+
+//Question 1
+
+// var caseQty
+
+//Question 2
+
+// var caseQty =
+// caseQty="144"
+// console.log(caseQty)
+
+//Question 3
+
+// var num = "9"
+// console.log(num)
+
+//Question 4
+
+// var total =
+// total = 9+4
+// console.log(total)
+
+//Question 5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// console.log( merchTotal + shippingCharge);
+
+//Question 6
+
+// var num = 9;
+// num1 = 7;
+// console.log(num*num1)
